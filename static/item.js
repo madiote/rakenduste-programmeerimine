950658aef1358ff1c5eeb7729bab9b399e190546
Madis <Madis0@users.noreply.github.com>
2019-09-08 16:51:27 +0300

    Task 7

--- static/item.js
+++ static/item.js
@@ -1,1 +1,8 @@
-alert("Hello world, item page!");
+const x = window.location;
+console.log(x);
+const urlParams = new URLSearchParams(window.location.search);
+const title = urlParams.get("title");
+const cost = urlParams.get("cost");
+const src = urlParams.get("src");
+console.log(title, cost, src);
+alert(`${title}\n${cost}€\n${src}`)
