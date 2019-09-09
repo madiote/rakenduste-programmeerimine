2ad1c5499e138d9625b95efa94b8d0d4616f7060
madiote <43170891+madiote@users.noreply.github.com>
2019-09-09 13:15:12 +0300

    Task 1

--- static/item.js
+++ static/item.js
@@ -5,4 +5,33 @@ const title = urlParams.get("title");
 const cost = urlParams.get("cost");
 const src = urlParams.get("src");
 console.log(title, cost, src);
-alert(`${title}\n${cost}€\n${src}`)
+//alert(`${title}\n${cost}€\n${src}`)
+
+const container = document.createElement("div");
+container.className = "itemContainer";
+
+const image = document.createElement("img");
+image.src = src;
+image.className = "item__image";
+
+const titleElement = document.createElement("h2");
+titleElement.textContent = title;
+titleElement.className = "item__title";
+
+const textElement = document.createElement("p");
+textElement.textContent = "Lorem ipsum dolor sit amet";
+textElement.className = "item__description";
+
+const costElement = document.createElement("p");
+costElement.textContent = cost;
+costElement.className = "item__price";
+
+container.append(titleElement);
+container.append(image);
+container.append(textElement);
+container.append(costElement);
+
+window.addEventListener("load", () => {
+    const app = document.getElementById("item-body");
+    app.append(container);
+});
