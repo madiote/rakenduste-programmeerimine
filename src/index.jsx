1348d936089ca5e7b20fe5b5b5565fe08b52c3bd
madiote <43170891+madiote@users.noreply.github.com>
2019-09-16 14:17:49 +0300

    Task 6

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,6 @@ import ReactDOM from "react-dom";
 const root = document.getElementById("app");
 
 ReactDOM.render(
-    React.createElement(
-        "button",
-        {},
-        "I am a button, hello!"
-    ),
+    <button>I am a button,  hello!</button>,
     root
 );
