1f42be504cbb5a97586c9f578c48f96d0342a24b
Madis <Madis0@users.noreply.github.com>
2019-10-05 21:17:24 +0300

    Task 6-7: mongodb

--- server/server.js
+++ server/server.js
@@ -3,6 +3,37 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model("Kitten", kittySchema);
+
+const kitten1 = new Kitten({
+    name: "Red cat 2",
+});
+
+const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-mvgj1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URL)
+.then(() => {
+    console.log("DB access successful");
+    kitten1.save(err => {
+        if(err){
+            console.log("Kitten error", err);
+        }
+        else {
+            console.log("Kitten success");
+        }
+    });
+})
+.catch(err => {
+    console.log("DB error", err);
+});
+
 
 /**
  * GET all items
