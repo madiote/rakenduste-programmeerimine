d2361c0e1e51676a4f56ac8c1c35f4b53b9ced99
Madis <Madis0@users.noreply.github.com>
2019-10-12 18:28:42 +0300

    Task 4

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import HomePage from "./pages/HomePage.jsx";
 import ItemPage from "./pages/ItemPage.jsx";
 import Header from "./components/Header.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import UserPage from "./pages/UserPage.jsx";
 import { BrowserRouter, Route } from "react-router-dom"; 
 
 const root = document.getElementById("app");
@@ -11,6 +14,9 @@ ReactDOM.render(
     <BrowserRouter>
         <Route path="/" component={Header} />
         <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/signup" exact component={SignupPage} />
+        <Route path="/users/:userId" exact component={UserPage} />
         <Route path="/items/:itemId" exact component={ItemPage} />
     </BrowserRouter>,
     root
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class UserPage extends React.PureComponent {
+    render(){
+        return (
+            <div>
+                user page
+            </div>
+        );
+    }
+}
+export default UserPage;
