d808d84b43ab193b18ad904647b21a2dfa115947
madiote <43170891+madiote@users.noreply.github.com>
2019-10-28 14:37:21 +0200

    Task 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,37 @@ import {Link} from "react-router-dom";
 import {profileIcon, cartIcon} from "../icons.js";
 import "./header.css";
 import PropTypes from "prop-types";
+import {AuthContext} from "../index.jsx";
 
-const Header = ({user}) => {
+const Header = () => {
     return (
-        <div className="header">
-            <Link to={"/"}>
-                <img src="/images/logo.svg" /> {/* className="header__logo" */}
-            </Link>
-            <div className="header__buttons">
-                {user.email && <WelcomeIcon user={user}/>}
-                {!user.email && <LoginRegisterIcon />}
+        <AuthContext.Consumer>
+            {
+                (contextValue) => (
+                    <div className="header">
+                        <Link to={"/"}>
+                            <img src="/images/logo.svg" /> {/* className="header__logo" */}
+                        </Link>
+                        <div className="header__buttons">
+                            {contextValue.user.email && <WelcomeIcon user={contextValue.user}/>}
+                            {!contextValue.user.email && <LoginRegisterIcon />}
 
-                <div className={"header__button"}>
-                    <img src={cartIcon} />
-                    <div className={"header__button-text"}>Cart</div>
-                </div>
-            </div>
-        </div>
+                            <div className={"header__button"}>
+                                <img src={cartIcon} />
+                                <div className={"header__button-text"}>Cart</div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </AuthContext.Consumer>
     );
 };
 
-Header.propTypes = {
+/* Header.propTypes = {
     token: PropTypes.string,
     user: PropTypes.object,
-};
+}; */
 
 const LoginRegisterIcon = () => (
     <Link className={"header__button"} to={"/login"}>
--- src/index.jsx
+++ src/index.jsx
@@ -9,15 +9,18 @@ import SignupPage from "./pages/SignupPage.jsx";
 import UserPage from "./pages/UserPage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
+const authDefaultValue = {
+    token: null,
+    user: {
+      email: null,
+      createdAt: null,
+      _id: null,
+    },
+};
+export const AuthContext = React.createContext(authDefaultValue);
+
 class App extends React.Component {
-    state = {
-        token: null,
-        user: {
-          email: null,
-          createdAt: null,
-          _id: null,
-        }
-      };
+    state = authDefaultValue;
 
     handleLogin = ({token, user}) => {
         this.setState({
@@ -27,39 +30,32 @@ class App extends React.Component {
 
     render(){
         return(
-            <BrowserRouter>
-            <Route 
-                path={"/"} 
-                render={(props) => 
-                    <Header 
-                        {...props} 
-                        token={this.state.token}
-                        user={this.state.user}
+            <AuthContext.Provider value={this.state}>
+                <BrowserRouter>
+                <Route path={"/"} component = {Header}/>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/login" 
+                        exact 
+                        render={(props) => 
+                            <LoginPage 
+                                {...props} 
+                                onLogin={this.handleLogin}
+                            />
+                        }  />
+                    <Route path="/signup" exact component={SignupPage} />
+                    <Route 
+                        path="/users/:userId" 
+                        exact 
+                        render={(props) => {
+                            return <UserPage {...props} user={this.state.user}/>;
+                        }}
                     />
-                } 
-            />
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/login" 
-                    exact 
-                    render={(props) => 
-                        <LoginPage 
-                            {...props} 
-                            onLogin={this.handleLogin}
-                        />
-                    }  />
-                <Route path="/signup" exact component={SignupPage} />
-                <Route 
-                    path="/users/:userId" 
-                    exact 
-                    render={(props) => {
-                        return <UserPage {...props} user={this.state.user}/>;
-                    }}
-                />
-                <Route path="/items/:itemId" exact component={ItemPage} />
-                <Route component={NotFound} />
-            </Switch>
-            </BrowserRouter>
+                    <Route path="/items/:itemId" exact component={ItemPage} />
+                    <Route component={NotFound} />
+                </Switch>
+                </BrowserRouter>
+            </AuthContext.Provider>
         );
     }
 }
