d449ac2d70ebd1080e41caad19b0c63e162a4986
Madis <Madis0@users.noreply.github.com>
2019-11-16 13:49:26 +0200

    Registration error fix

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -26,7 +26,10 @@ class SignupPage extends React.PureComponent {
             },
             body: JSON.stringify(this.state)
         })
-        .then(res => res.json())
+        .then(res => {
+            if(!res.ok) throw "Signup failed";
+            return res.json();
+        })
         .then(() => {
             this.props.history.push("/login");
             toast.success("Registreerumine oli edukas! :)");
