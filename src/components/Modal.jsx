493a32595a9cf2f68292fa64dd4fe42a1e23b1f8
madiote <43170891+madiote@users.noreply.github.com>
2019-11-25 13:49:58 +0200

    Task 1 wip

---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Modal = ({children, open, onClose}) => (
+    <div className={"modal"}>
+        <div className={"modal__inner"}>
+
+        </div>
+    </div>
+);
+
+export default Modal;
